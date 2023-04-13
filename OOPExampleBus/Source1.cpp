1f2496a4a6ad1e0e0bde550db3f89472da199ab0
Igor <igara8880@gmail.com>
2023-04-13 18:22:29 +0300

    Fix bugs

--- OOPExampleBus/Source1.cpp
+++ OOPExampleBus/Source1.cpp
@@ -6,7 +6,7 @@ class Bus {
 public:
 	string numer;
 	string Type;
-	int cost;
+	float cost;
 	int passenger_number;
 	float benefit;
 
@@ -22,28 +22,28 @@ public:
 		Type = Type1;
 		cost = cost1;
 		passenger_number = pass_num;
-		benefit = cost / pass_num;
+		benefit = (cost / pass_num);
 	}
 	
 	Bus Auto_Type() {
 		string type;
 		Bus type1;
-		type1.Type == type;
+		type1.Type = type;
 		int num;
 		num = rand() % 5;
 		switch (num) {
-		case 0: type == "MAZ"; break;
-		case 1: type == "PAZ"; break;
-		case 2: type == "Mersedes Benz"; break;
-		case 3: type == "Scania"; break;
-		case 4: type == "Neoplan"; break;
+		case 0: type = "MAZ"; break;
+		case 1: type = "PAZ"; break;
+		case 2: type = "Mersedes Benz"; break;
+		case 3: type = "Scania"; break;
+		case 4: type = "Neoplan"; break;
 		}
 		return type1;
 	}
 	Bus Auto_Cost() {
 		int num;
 		Bus bus;
-		num == rand() + 10000;
+		num = (rand() + 10000);
 		bus.cost == num;
 		return bus;
 	}
@@ -52,6 +52,7 @@ public:
 		Bus bus;
 		num = (rand() % 85) + 15;
 		bus.passenger_number = num;
+		return bus;
 	}
 	Bus Auto_Benefit(int cost, int pass_num) {
 		Bus bus;
@@ -61,29 +62,29 @@ public:
 	Bus Auto_Numer() {
 		Bus bus;
 		string numer;
-		int num = rand() % 100;
+		int num = rand() % 20;
 		switch (num) {
-		case 0: numer == "0000PM7"; break;
-		case 1: numer == "0001PM7"; break;
-		case 2: numer == "0002PM7"; break;
-		case 3: numer == "0003PM7"; break;
-		case 4: numer == "0004PM7"; break;
-		case 5: numer == "0005PM7"; break;
-		case 6: numer == "0006PM7"; break;
-		case 7: numer == "0007PM7"; break;
-		case 8: numer == "0008PM7"; break;
-		case 9: numer == "0009PM7"; break;
-		case 10: numer == "0010PM7"; break;
-		case 11: numer == "0011PM7"; break;
-		case 12: numer == "0012PM7"; break;
-		case 13: numer == "0013PM7"; break;
-		case 14: numer == "0014PM7"; break;
-		case 15: numer == "0015PM7"; break;
-		case 16: numer == "0016PM7"; break;
-		case 17: numer == "0017PM7"; break;
-		case 18: numer == "0018PM7"; break;
-		case 19: numer == "0019PM7"; break;
-		case 20: numer == "0020PM7"; break;
+		case 0: numer = "0000PM7"; break;
+		case 1: numer = "0001PM7"; break;
+		case 2: numer = "0002PM7"; break;
+		case 3: numer = "0003PM7"; break;
+		case 4: numer = "0004PM7"; break;
+		case 5: numer = "0005PM7"; break;
+		case 6: numer = "0006PM7"; break;
+		case 7: numer = "0007PM7"; break;
+		case 8: numer = "0008PM7"; break;
+		case 9: numer = "0009PM7"; break;
+		case 10: numer = "0010PM7"; break;
+		case 11: numer = "0011PM7"; break;
+		case 12: numer = "0012PM7"; break;
+		case 13: numer = "0013PM7"; break;
+		case 14: numer = "0014PM7"; break;
+		case 15: numer = "0015PM7"; break;
+		case 16: numer = "0016PM7"; break;
+		case 17: numer = "0017PM7"; break;
+		case 18: numer = "0018PM7"; break;
+		case 19: numer = "0019PM7"; break;
+		case 20: numer = "0020PM7"; break;
 		}
 		bus.numer = numer;
 		return bus;
@@ -97,6 +98,7 @@ public:
 		int cost = bus.cost;
 		int pass_num = bus.passenger_number;
 		bus.Auto_Benefit(cost, pass_num);
+		return bus;
 	}
 	string convert() {
 		string msg = " ";
@@ -108,8 +110,21 @@ public:
 
 int main() {
 	srand(time(NULL));
-
-
+	Bus bus1;
+	Bus bus2;
+	Bus bus3;
+	Bus bus4;
+	Bus bus5;
+	bus1.Iniciate();
+	bus2.Iniciate();
+	bus3.Iniciate();
+	bus4.Iniciate();
+	bus5.Iniciate();
+	cout << "Bus1" << bus1.convert() << endl;
+	cout << "Bus2" << bus2.convert() << endl;
+	cout << "Bus3" << bus3.convert() << endl;
+	cout << "Bus4" << bus4.convert() << endl;
+	cout << "Bus5" << bus5.convert() << endl;
 
 
 
